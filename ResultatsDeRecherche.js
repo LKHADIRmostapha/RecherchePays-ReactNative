2a6e8f912a4ec762679ed66254339b03f481b976
ElMehdiEchchad <58845064+ElMehdiEchchad@users.noreply.github.com>
2021-06-27 16:55:26 +0100

    Update ResultatsDeRecherche.js

--- ResultatsDeRecherche.js
+++ ResultatsDeRecherche.js
@@ -6,22 +6,29 @@ import {
  TouchableHighlight,
  FlatList,
  Text,
+ ListItem
 } from 'react-native';
 
 type Props = {};
 export default class ResultatsDeRecherche extends Component<Props> {
  _extracteurClef = (item, index) => index.toString();
- _rendreItem = ({item}) => {
- return (
- <TouchableHighlight
- underlayColor='#ddddda'>
- <View>
- <Text>{item.name}</Text>
- </View>
- </TouchableHighlight>
- );
 
- };
+_rendreItem = ({item, index}) => (
+ <ListItem
+ item={item}
+ index={index}
+ onPressItem={this._itemAppuye}
+ />
+ 
+);
+
+_itemAppuye = (index) => {
+ console.log('Ligne appuyée : '+index);
+}; 
+
+
+
+
  render() {
  console.log(this.props.route.params);
  const { listings } = this.props.route.params;
@@ -34,3 +41,26 @@ export default class ResultatsDeRecherche extends Component<Props> {
  );
  }
 }
+const styles = StyleSheet.create({
+ conteneurTexte: {
+ flex: 1
+ },
+ separateur: {
+ height: 1,
+ backgroundColor: '#eedded'
+ },
+ nom: {
+ fontSize: 25,
+ fontWeight: 'bold',
+ color: '#58BEEC'
+ },
+ region: {
+ fontSize: 20,
+ color: '#656565'
+ },
+ conteneurLigne: {
+ flexDirection: 'row',
+ padding: 10
+ },
+});
+
